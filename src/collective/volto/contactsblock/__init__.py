c0cc2ee2318f6ca09827b6be7465e9f63fdc0377
Roman Kysil <roman.kysil@redturtle.it>
2025-09-05 15:47:02 +0200

    Initial commit

--- src/collective/volto/contactsblock/__init__.py
+++ src/collective/volto/contactsblock/__init__.py
@@ -3,4 +3,4 @@
 from zope.i18nmessageid import MessageFactory
 
 
-_ = MessageFactory('collective.volto.contactsblock')
+_ = MessageFactory("collective.volto.contactsblock")
--- src/collective/volto/contactsblock/interfaces.py
+++ src/collective/volto/contactsblock/interfaces.py
@@ -2,7 +2,20 @@
 """Module where all interfaces, events and exceptions live."""
 
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from plone.restapi.controlpanels.interfaces import IControlpanel
+from zope.schema import SourceText
+from collective.volto.contactsblock import _
+import json
 
 
 class ICollectiveVoltoContactsblockLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+
+class IContactsBlock(IControlpanel):
+    contact_configuration = SourceText(
+        title=_("contacts_configuration_label", default="Contacts configuration"),
+        description="",
+        required=True,
+        default=json.dumps([{"rootPath": "/", "items": []}]),
+    )
