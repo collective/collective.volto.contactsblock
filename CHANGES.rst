a0e474654158cb1bab36e36a61360e1470c06d74
Luca Bellenghi <lucabel@gmail.com>
2025-10-02 15:29:46 +0200

    Preparing release 1.0.1

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,7 +2,7 @@ Changelog
 =========
 
 
-1.0.1 (unreleased)
+1.0.1 (2025-10-02)
 ------------------
 
 - Fix Manifest
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="collective.volto.contactsblock",
-    version="1.0.1.dev0",
+    version="1.0.1",
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
